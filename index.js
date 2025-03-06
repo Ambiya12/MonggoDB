b4a4e52d3412f2c27aa841c62606262927c4bb66
Ambiya12 <ambiyadms@gmail.com>
2025-03-06 15:57:34 +0100

    Add JSON parsing middleware and root endpoint for API

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ import usersRouter from './routes/usersRouter.js';
 
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', usersRouter);
 
-  connectDB();
+app.get('/', (req, res) => {
+    res.send('Helloo Welcome to Usersss API');
+});
 
+connectDB();
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
