a3fc62574332a788a04c79647bd16cb4e6fb2696
Ambiya12 <ambiyadms@gmail.com>
2025-03-07 16:13:53 +0100

    Add authentication router to main application

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express, { application } from 'express';
 import connectDB from './client/db.js';
 import usersRouter from './routes/usersRouter.js';
 import recipesRouter from './routes/recipesRouter.js';
-
+import authRouter from './routes/authRouter.js';
 
 const app = express();
 
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', usersRouter);
 
 app.use('/api', recipesRouter);
+
+app.use('/api', authRouter);
 
 app.get('/', (req, res) => {
     res.send('Helloo Welcome to Usersss API');
